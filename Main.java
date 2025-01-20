50f179fb9d5956317b442cdf551cebca43502e5b
Olahushftw <adeshinaolamide979@gmail.com>
2025-01-20 19:07:36 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String row = "* * * * * = = = = = * * * * *\n";
+        for (int i = 0; i < 5; i++) { // Assuming 5 rows
+            System.out.print(row);
+        }
+    }
+}
